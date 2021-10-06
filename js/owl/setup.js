fb86e7ae94502620cb7d9645ae32321cd966e28e
Lucas Gonçalves <front.end.lb@gmail.com>
2021-10-06 14:21:27 -0300

    initial commit

---
+++ js/owl/setup.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:4,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        },
+        1200:{
+            items:7
+        }
+    }
+})
